0dbe9d769dfafea7b2e17fdc38a771bd47795934
Johnny Neagoe <johnnyneagoe@gmail.com>
2018-09-12 00:07:43 -0400

    Initial Set up (Not Working)

---
+++ models/events.js
@@ -0,0 +1,19 @@
+module.exports = function(sequelize, DataTypes) {
+  var Events = sequelize.define("Events", {
+    eventName: DataTypes.STRING,
+    eventDescription: DataTypes.STRING,
+    eventType: DataTypes.STRING,
+    eventAuthor: DataTypes.STRING,
+    eventLocation: DataTypes.STRING,
+    eventComments: DataTypes.STRING
+  });
+
+  // Events.associate = function(models) {
+  //   Events.belongsTo(models.Users, {
+  //     foreignKey: {
+  //       allowNull: false
+  //     }
+  //   });
+  // };
+  return Events;
+};
