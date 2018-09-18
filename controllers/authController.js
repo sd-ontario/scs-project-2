403e2b029c9b13bc877dc589feb5bed67f364d73
Stephen <stephen.dawe94@gmail.com>
2018-09-18 19:20:23 -0400

    Attempted to add passport

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,36 @@
 var exports = module.exports = {}
 
-exports.landingpage = function(req,res) {
-    res.render('landingpage');
+
+exports.landingpage = function(req, res){
+  res.render("landingpage");
 }
 
-exports.signup = function(req,res) {
-    res.render('signup');
+
+
+exports.signup = function(req,res){
+
+	res.render('signup'); 
+
 }
 
-exports.signin = function(req, res) {
-    res.render('signin');
+exports.signin = function(req,res){
+
+	res.render('signin'); 
+
 }
+
+exports.dashboard = function(req,res){
+
+	res.render('dashboard'); 
+
+}
+
+exports.logout = function(req,res){
+
+  req.session.destroy(function(err) {
+  res.redirect('/');
+  });
+
+}
+
+
