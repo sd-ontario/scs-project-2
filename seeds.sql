df78cc8d289855d4469c69c9ac8ca16e3c6ceccc
AnumShabbir <anumsh89@hotmail.com>
2018-09-19 00:08:41 -0400

    meet your neighbor page

---
+++ seeds.sql
@@ -0,0 +1,146 @@
+
+
+
+INSERT INTO `exampledb`.`users` (`id`, `firstname`, `lastname`, `username`, `postalcode`, `about`, `email`, `password`, `last_login`, `status`, `createdAt`, `updatedAt`) 
+VALUES ('1', 'Anne', 'V', 'anne', 'L4E5B3', 'UI Developer', 'anne@email.com', '12345', '2018-09-19 02:05:07', 'active', '2018-09-19 02:05:07', '2018-09-19 02:05:07');
+
+
+INSERT INTO `exampledb`.`users` (`id`, `firstname`, `lastname`, `username`, `postalcode`, `about`, `email`, `password`, `last_login`, `status`, `createdAt`, `updatedAt`) 
+VALUES ('2', 'Anum', 'Shabbir', 'anum', 'L4E5B3', 'QA Engieer', 'anum@email.com', '12345', '2018-09-19 02:05:07', 'active', '2018-09-19 02:05:07', '2018-09-19 02:05:07');
+
+
+INSERT INTO `exampledb`.`users` (`id`, `firstname`, `lastname`, `username`, `postalcode`, `about`, `email`, `password`, `last_login`, `status`, `createdAt`, `updatedAt`) 
+VALUES ('3', 'Johnny','Neagoe', 'johnny', 'L4E5B3', 'Accountant', 'johnny@email.com', '12345', '2018-09-19 02:05:07', 'active', '2018-09-19 02:05:07', '2018-09-19 02:05:07');
+
+
+INSERT INTO `exampledb`.`users` (`id`, `firstname`, `lastname`, `username`, `postalcode`, `about`, `email`, `password`, `last_login`, `status`, `createdAt`, `updatedAt`) 
+VALUES ('4', 'Stephen', 'Dawe', 'stephen', 'L4E5B3', 'Developer', 'stephen@email.com', '12345', '2018-09-19 02:05:07', 'active', '2018-09-19 02:05:07', '2018-09-19 02:05:07');
+
+
+INSERT INTO `exampledb`.`users` (`id`, `firstname`, `lastname`, `username`, `postalcode`, `about`, `email`, `password`, `last_login`, `status`, `createdAt`, `updatedAt`)
+ VALUES ('5', 'Tom', 'Chen', 'tom', 'M5S1W7', 'Web Developer', 'tom@email.com', '12345', '2018-09-19 02:05:07', 'active', '2018-09-19 02:05:07', '2018-09-19 02:05:07');
+
+
+INSERT INTO `exampledb`.`users` (`id`, `firstname`, `lastname`, `username`, `postalcode`, `about`, `email`, `password`, `last_login`, `status`, `createdAt`, `updatedAt`) 
+VALUES ('6', 'Albert', 'Burchi', 'albert', 'M5S1W7', 'Web Developer', 'albert@email.com', '12345', '2018-09-19 02:05:07', 'active', '2018-09-19 02:05:07', '2018-09-19 02:05:07');
+
+
+INSERT INTO `exampledb`.`users` (`id`, `firstname`, `lastname`, `username`, `postalcode`, `about`, `email`, `password`, `last_login`, `status`, `createdAt`, `updatedAt`)
+ VALUES ('7', 'Edward', 'Apostol', 'edward', 'M5S1W7', 'Instructor', 'edward@email.com', '12345', '2018-09-19 02:05:07', 'active', '2018-09-19 02:05:07', '2018-09-19 02:05:07');
+
+
+INSERT INTO `exampledb`.`users` (`id`, `firstname`, `lastname`, `username`, `postalcode`, `about`, `email`, `password`, `last_login`, `status`, `createdAt`, `updatedAt`) 
+VALUES ('8', 'John', 'Seh', 'John', 'M5V2L7', 'Chef', 'john@email.com', '12345', '2018-09-19 02:05:07', 'active', '2018-09-19 02:05:07', '2018-09-19 02:05:07');
+
+
+INSERT INTO `exampledb`.`users` (`id`, `firstname`, `lastname`, `username`, `postalcode`, `about`, `email`, `password`, `last_login`, `status`, `createdAt`, `updatedAt`) 
+VALUES ('9', 'Andy', 'Blake', 'andy', 'M5V2L7', 'Program Manager', 'andy@email.com', '12345', '2018-09-19 02:05:07', 'active', '2018-09-19 02:05:07', '2018-09-19 02:05:07');
+
+
+INSERT INTO `exampledb`.`users` (`id`, `firstname`, `lastname`, `username`, `postalcode`, `about`, `email`, `password`, `last_login`, `status`, `createdAt`, `updatedAt`) 
+VALUES ('10', 'Sara', 'Miller', 'sara', 'M5V2L7', 'Pilot', 'sara@email.com', '12345', '2018-09-19 02:05:07', 'active', '2018-09-19 02:05:07', '2018-09-19 02:05:07');
+
+
+
+INSERT INTO `exampledb`.`posts` (`id`, `postTitle`, `postBody`, `User_ID`, `postalCode`, `createdAt`, `updatedAt`) 
+VALUES ('1', 'Help for moving', 'Hello, I need some help with moving furniture  Saturday, around 5pm', '1', 'L4E5B3', '2018-09-19 02:05:07', '2018-09-19 02:05:07');
+
+
+
+INSERT INTO `exampledb`.`posts` (`id`, `postTitle`, `postBody`, `User_ID`, `postalCode`, `createdAt`, `updatedAt`) 
+VALUES ('2', 'Walk my dog', 'Hey, I am not feeling that well, if somebody can talk my dog for walk, that would be great', '2', 'L4E5B3', '2018-09-19 02:05:07', '2018-09-19 02:05:07');
+
+
+
+
+INSERT INTO `exampledb`.`posts` (`id`, `postTitle`, `postBody`, `User_ID`, `postalCode`, `createdAt`, `updatedAt`) 
+VALUES ('3', 'Carpooling', 'Going for meeting to London Ontario, contact me for car pooling', '1', 'L4E5B3', '2018-09-19 02:05:07', '2018-09-19 02:05:07');
+
+
+
+
+INSERT INTO `exampledb`.`posts` (`id`, `postTitle`, `postBody`, `User_ID`, `postalCode`, `createdAt`, `updatedAt`) 
+VALUES ('4', 'Electricity Issue', 'Did somebody else had issue with lights flickering?', '3', 'L4E5B3', '2018-09-19 02:05:07', '2018-09-19 02:05:07');
+
+
+
+
+INSERT INTO `exampledb`.`posts` (`id`, `postTitle`, `postBody`, `User_ID`, `postalCode`, `createdAt`, `updatedAt`) 
+VALUES ('5', 'Car Parking', 'License plate:AAA-111, please adjust your car parking on the street', '4', 'L4E5B3', '2018-09-19 02:05:07', '2018-09-19 02:05:07');
+
+
+
+
+INSERT INTO `exampledb`.`posts` (`id`, `postTitle`, `postBody`, `User_ID`, `postalCode`, `createdAt`, `updatedAt`) 
+VALUES ('6', 'Garbage Day', 'Is this garbage or recycle day, this week?', '3', 'L4E5B3', '2018-09-19 02:05:07', '2018-09-19 02:05:07');
+
+
+
+
+INSERT INTO `exampledb`.`posts` (`id`, `postTitle`, `postBody`, `User_ID`, `postalCode`, `createdAt`, `updatedAt`) 
+VALUES ('7', 'Sale at Home Sense this weekend', 'There is sales at Home Sense of 25% everything! ', '4', 'L4E5B3', '2018-09-19 02:05:07', '2018-09-19 02:05:07');
+
+
+
+
+INSERT INTO `exampledb`.`posts` (`id`, `postTitle`, `postBody`, `User_ID`, `postalCode`, `createdAt`, `updatedAt`) 
+VALUES ('8', 'Printer', 'Does somebody has color printer ? Need it urgently!', '3', 'L4E5B3', '2018-09-19 02:05:07', '2018-09-19 02:05:07');
+
+
+
+
+
+INSERT INTO `exampledb`.`posts` (`id`, `postTitle`, `postBody`, `User_ID`, `postalCode`, `createdAt`, `updatedAt`) 
+VALUES ('9', 'Hey Neighbours!', 'I just moved in yesterday in this community, would love to meet you all! ', '2', 'L4E5B3', '2018-09-19 02:05:07', '2018-09-19 02:05:07');
+
+
+
+INSERT INTO `exampledb`.`events` (`id`, `eventName`, `eventDescription`, `eventType`, `eventLocation`, `User_ID`, `postalCode`, `createdAt`, `updatedAt`) VALUES 
+('5', 'Halloween Decoration', 'We are doing halloween decorations in the neighbourhood, join in the preparation discussion on Friday, 7pm', 'Meeting', 'Wheatsheaf Street, Richmond Hill, ON, Canada', '2', 'L4E5B3', '2018-09-19 02:39:09', '2018-09-19 02:39:09');
+
+
+
+
+INSERT INTO `exampledb`.`events` (`id`, `eventName`, `eventDescription`, `eventType`, `eventLocation`, `User_ID`, `postalCode`, `createdAt`, `updatedAt`) 
+VALUES 
+('1', 'Barbecue Party', 'Summer barbeque at Tadpole Parkette, come  join in everybody!', 'Party', 'Wheatsheaf Street, Richmond Hill, ON, Canada', '2', 'L4E5B3', '2018-09-19 02:39:09', '2018-09-19 02:39:09');
+
+
+
+
+INSERT INTO `exampledb`.`events` (`id`, `eventName`, `eventDescription`, `eventType`, `eventLocation`, `User_ID`, `postalCode`, `createdAt`, `updatedAt`) 
+VALUES 
+('2', 'Soccer match', 'Players needed for Soccer Match on Sunday, 6pm.', 'Sports', 'Wheatsheaf Street, Richmond Hill, ON, Canada', '4', 'L4E5B3', '2018-09-19 02:39:09', '2018-09-19 02:39:09');
+
+
+
+
+INSERT INTO `exampledb`.`events` (`id`, `eventName`, `eventDescription`, `eventType`, `eventLocation`, `User_ID`, `postalCode`, `createdAt`, `updatedAt`) 
+VALUES 
+('3', 'Community Meeting', 'Monthly Community Meeting on Sunday at  10am', 'Meeting', 'Wheatsheaf Street, Richmond Hill, ON, Canada', '1', 'L4E5B3', '2018-09-19 02:39:09', '2018-09-19 02:39:09');
+
+
+
+
+INSERT INTO `exampledb`.`events` (`id`, `eventName`, `eventDescription`, `eventType`, `eventLocation`, `User_ID`, `postalCode`, `createdAt`, `updatedAt`) 
+VALUES 
+('4', 'Halloween Decoration', 'We are doing halloween decorations in the neighbourhood, join in the preparation discussion on Friday, 7pm', 'Meeting', 'Wheatsheaf Street, Richmond Hill, ON, Canada', '3', 'L4E5B3', '2018-09-19 02:39:09', '2018-09-19 02:39:09');
+
+
+
+
+
+INSERT INTO `exampledb`.`events` (`id`, `eventName`, `eventDescription`, `eventType`, `eventLocation`, `User_ID`, `postalCode`, `createdAt`, `updatedAt`) 
+VALUES 
+('5', 'Meet & Greet', 'Meet & Greet and some snacks for all neigbours, Saturday 3pm', 'Party', 'Wheatsheaf Street, Richmond Hill, ON, Canada', '1', 'L4E5B3', '2018-09-19 02:39:09', '2018-09-19 02:39:09');
+
+
+
+
+INSERT INTO `exampledb`.`events` (`id`, `eventName`, `eventDescription`, `eventType`, `eventLocation`, `User_ID`, `postalCode`, `createdAt`, `updatedAt`) 
+VALUES 
+('6', 'Clean Park', 'Come join us to take part in cleaning event of the Tadpole Park', 'Meeting', 'Wheatsheaf Street, Richmond Hill, ON, Canada', '2', 'L4E5B3', '2018-09-19 02:39:09', '2018-09-19 02:39:09');
+
+
+
+
